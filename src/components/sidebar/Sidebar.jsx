0c7c0ec0284ce70b5e1927a6bac2394a8fdfa996
unknown <johnmsn22@gmail.com>
2023-03-28 05:05:26 +0100

    sidebar menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,52 +1,98 @@
 import React from "react";
-import {NavLink, Link} from 'react-router-dom' 
-import './sidebar.scss'
+import { NavLink, Link } from "react-router-dom";
+import "./sidebar.scss";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <div className='wrapper'>
+    <div className="sidebar">
+      <div className="wrapper">
         <div>
           <p>Switch Organization</p>
-          <p><NavLink to='/'>Dashboard</NavLink></p>
+          <ul>
+            <li>
+              <NavLink to="/">Dashboard</NavLink>
+            </li>
+          </ul>
         </div>
 
         <div>
           <h4>customers</h4>
           <ul>
-            <li><NavLink to='users'>Users</NavLink></li>
-            <li><NavLink>Guarantors</NavLink></li>
-            <li><NavLink>Loans</NavLink></li>
-            <li><NavLink>Decision Models</NavLink></li>
-            <li><NavLink>Savings</NavLink></li>
-            <li><NavLink>Loan Requests</NavLink></li>
-            <li><NavLink>Whitelist</NavLink></li>
-            <li><NavLink>Karma</NavLink></li>
+            <li>
+              <NavLink to="users">Users</NavLink>
+            </li>
+            <li>
+              <NavLink>Guarantors</NavLink>
+            </li>
+            <li>
+              <NavLink>Loans</NavLink>
+            </li>
+            <li>
+              <NavLink>Decision Models</NavLink>
+            </li>
+            <li>
+              <NavLink>Savings</NavLink>
+            </li>
+            <li>
+              <NavLink>Loan Requests</NavLink>
+            </li>
+            <li>
+              <NavLink>Whitelist</NavLink>
+            </li>
+            <li>
+              <NavLink>Karma</NavLink>
+            </li>
           </ul>
         </div>
 
         <div>
-        <h4>businesses</h4>
+          <h4>businesses</h4>
           <ul>
-            <li><NavLink>Organization</NavLink></li>
-            <li><NavLink>Loan Products</NavLink></li>
-            <li><NavLink>Savings Products</NavLink></li>
-            <li><NavLink>Fees and Charges</NavLink></li>
-            <li><NavLink>Transactions</NavLink></li>
-            <li><NavLink>Services</NavLink></li>
-            <li><NavLink>Service Account</NavLink></li>
-            <li><NavLink>Settlements</NavLink></li>
-            <li><NavLink>Reports</NavLink></li>
+            <li>
+              <NavLink>Organization</NavLink>
+            </li>
+            <li>
+              <NavLink>Loan Products</NavLink>
+            </li>
+            <li>
+              <NavLink>Savings Products</NavLink>
+            </li>
+            <li>
+              <NavLink>Fees and Charges</NavLink>
+            </li>
+            <li>
+              <NavLink>Transactions</NavLink>
+            </li>
+            <li>
+              <NavLink>Services</NavLink>
+            </li>
+            <li>
+              <NavLink>Service Account</NavLink>
+            </li>
+            <li>
+              <NavLink>Settlements</NavLink>
+            </li>
+            <li>
+              <NavLink>Reports</NavLink>
+            </li>
           </ul>
         </div>
 
         <div>
-        <h4>settings</h4>
+          <h4>settings</h4>
           <ul>
-            <li><NavLink>Preferences</NavLink></li>
-            <li><NavLink>Fees and Pricing</NavLink></li>
-            <li><NavLink>Audit Logs</NavLink></li>
-            <li><NavLink>Systems Messages</NavLink></li>
+            <li>
+              <NavLink>Preferences</NavLink>
+            </li>
+            <li>
+              <NavLink>Fees and Pricing</NavLink>
+            </li>
+            <li>
+              <NavLink>Audit Logs</NavLink>
+            </li>
+            <li>
+              <NavLink>Systems Messages</NavLink>
+            </li>
           </ul>
         </div>
 
@@ -56,7 +102,7 @@ const Sidebar = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Sidebar;
